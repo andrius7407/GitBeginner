c7d126eb63324227cc012f016487df44218e787c
andrius7407 <andrius.el@gmail.com>
2019-02-21 21:59:34 +0200

    my first commit

---
+++ ArrOfPersons.java
@@ -0,0 +1,11 @@
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.Random;
+
+public class ArrOfPersons {
+	
+	public static void main(String[] args) {
+		
+		Person1[] person1 = new Person1[10];
+			
+	}
+}
