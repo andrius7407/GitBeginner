c911ce283cdc3ac93eadf1b893780e286323b11f
andrius7407 <andrius.el@gmail.com>
2019-02-22 05:49:12 +0200

    new commit

--- ArrOfPersons.java
+++ ArrOfPersons.java
@@ -18,18 +18,6 @@ public class ArrOfPersons {
 	
 	public static String generateRandomString() {
   
-		int leftLimit = 97; // letter 'a'
-		int rightLimit = 122; // letter 'z'
-		int targetStringLength = 5;
-		Random random = new Random();
-		StringBuilder buffer = new StringBuilder(targetStringLength);
-		for (int i = 0; i < targetStringLength; i++) {
-			int randomLimitedInt = leftLimit + (int) 
-			  (random.nextFloat() * (rightLimit - leftLimit + 1));
-			buffer.append((char) randomLimitedInt);
-		}
-		String generatedString = buffer.toString();
-		return generatedString;
-		//System.out.println(generatedString);
+		
 	}
 }
